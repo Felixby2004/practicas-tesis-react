3de69caba7b24d6123ae2a97c742b69323ca74c9
Felixby2004 <andree.felix2004@gmail.com>
2026-05-03 13:03:10 -0500

    Fix: Arreglo 2

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -3,10 +3,7 @@ import 'dotenv/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
   datasource: {
-    url: process.env.postgresql://neondb_owner:npg_OcDdJZK56TrE@ep-falling-glade-annwof6w-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require!,
+    url: process.env.DATABASE_URL,
   },
 });
